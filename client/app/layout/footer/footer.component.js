85be5e64c7562b12fbbd0564208a5c3d87c81581
Chuck <cfunkles@gmail.com>
2017-08-26 16:46:44 -0600

    fix element placements

--- client/app/layout/footer/footer.component.js
+++ client/app/layout/footer/footer.component.js
@@ -15,57 +15,53 @@ const footerComponent = {
 				Email<br>
 				<input type="email" placeholder="Email Address"><br>
 				Message<br>
-				<textarea type="text"></textarea>
+				<textarea type="text"></textarea><br>
 				<button class="submit">Submit</button>
 			</form>
-			<div class="contactSectionRight">
-				<div class="hours">
-					<h2>Hours</h2>
-					<table>
-						<tbody>
-							<tr>
-								<td>Mon<span>day</span></td>
-								<td>Closed</td>
-							</tr>
-							<tr>
-								<td>Tue<span>sday</span></td>
-								<td>Closed</td>
-							</tr>
-							<tr>
-								<td>Wed<span>nesday</span></td>
-								<td>11:00am - 7:00pm</td>
-							</tr>
-							<tr>
-								<td>Thur<span>sday</span></td>
-								<td>11:00am - 7:00pm</td>
-							</tr>
-							<tr>
-								<td>Fri<span>day</span></td>
-								<td>11:00am - 8:00pm</td>
-							</tr>
-							<tr>
-								<td>Sat<span>urday</span></td>
-								<td>11:00am - 8:00pm</td>
-							</tr>
-							<tr>
-								<td>Sun<span>day</span></td>
-								<td>11:00am - 5:00pm</td>
-							</tr>
-						</tbody>
-					</table>
-					<div class="businessAddress">
-						<h4>Rowdy Mermain Kombucha</h4>
-						<p>2516 49th ST, Unit 2</p>
-						<p>Boulder, CO</p>
-						<p>(303) 396-0498</p>
-					</div>
+		</div>
+		<div class="contactSectionRight">
+			<div class="contactHours">
+				<h2>Hours</h2>
+				<table>
+					<tbody>
+						<tr>
+							<td>Mon<span>day</span></td>
+							<td>Closed</td>
+						</tr>
+						<tr>
+							<td>Tue<span>sday</span></td>
+							<td>Closed</td>
+						</tr>
+						<tr>
+							<td>Wed<span>nesday</span></td>
+							<td>11:00am - 7:00pm</td>
+						</tr>
+						<tr>
+							<td>Thur<span>sday</span></td>
+							<td>11:00am - 7:00pm</td>
+						</tr>
+						<tr>
+							<td>Fri<span>day</span></td>
+							<td>11:00am - 8:00pm</td>
+						</tr>
+						<tr>
+							<td>Sat<span>urday</span></td>
+							<td>11:00am - 8:00pm</td>
+						</tr>
+						<tr>
+							<td>Sun<span>day</span></td>
+							<td>11:00am - 5:00pm</td>
+						</tr>
+					</tbody>
+				</table>
+				<div class="contactAddress">
+					<h4>Rowdy Mermaid Kombucha</h4>
+					<p>2516 49th ST, Unit 2</p>
+					<p>Boulder, CO</p>
+					<p>(303) 396-0498</p>
 				</div>
-			<div>
-		</div>
-		<div class="contactHours">
-		</div>
-		<div class="contactAddress">
-		</div>
+			</div>
+		<div>
 	</section>
 	<footer class="container-footer">
 		<div class="copyright-text">
