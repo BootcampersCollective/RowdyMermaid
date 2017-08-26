97fdd63fd04085a76a7f85b9a27a460137aee769
Dan Ladendorf <dladendorf@gmail.com>
2017-08-26 13:51:28 -0600

    4-Header-Style: Auto stash before merge of "4-Header-Style" and "development"

--- client/app/layout/header/header.component.js
+++ client/app/layout/header/header.component.js
@@ -1,16 +1,17 @@
 const headerComponent = {
 	bindings: {},
-	controller: function () {
-
-	},
+	controller: function() {},
 	template: `<div class="container-menu">
-<div>Header</div>
+<div class="container-header">
+	<div class="header-text">Header</div>
+</div>
 		<img class="logo" src="images/rowdyMermaidWordyLogo.png" alt="Rowdy Mermaid Logo">
     <rowdy-menu-bar></rowdy-menu-bar>
 </div>`
 };
 
-angular.module('RowdyMermaid-site.layout')
+angular
+	.module('RowdyMermaid-site.layout')
 	.component('headerComponent', headerComponent);
 
 headerComponent.$inject = [];
