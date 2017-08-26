c6ace335eef1b9e53d74c126910465b67ce0553b
Chuck <cfunkles@gmail.com>
2017-08-26 12:46:40 -0600

    update tag for city moved page title out of div

--- client/app/home/components/home.component.js
+++ client/app/home/components/home.component.js
@@ -12,11 +12,11 @@ const home = {
 		ctrl.$postLink = function () {};
 	},
 	template: `
+	<h1>This is the home page</h1>
 	<div class="siteName">
 		<img src="images/bottleLivingGingerCircleFrame.png"/>
-		<h1>This is the home page</h1>
-		<h3>ROWDY MERMAID KOMBUCHA</h3>
-		<h3>BOULDER, COLORADO</h3>
+		<h3>Rowdy Mermaid Kombucha</h3>
+		<h4>Boulder, Colorado</h4>
 	</div>
 	`
 
