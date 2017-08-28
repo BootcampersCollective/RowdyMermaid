cee93b2696627c4db02588fe62f8483ad3cf9d04
RJ Russell <RJ-Russell@users.noreply.github.com>
2017-08-28 01:43:31 -0600

    rework menu bar so that it is in a presentable state

--- client/app/widgets/menubutton/menubutton.directive.js
+++ client/app/widgets/menubutton/menubutton.directive.js
@@ -1,21 +1,23 @@
 function rowdyMenuButton() {
-	const directive = {
-		link: link,
-		restrict: 'EA',
-		template: `<div class="menu-button">
-    <a ui-sref="{{button.sref}}" ng-class="{'icon': button.iconClass}">
-       <i ng-if="button.iconClass" ng-class="button.iconClass"class="fa"></i>
-       <span>{{button.name}}</span>
-    </a>
-</div>`,
-	};
-	return directive;
+  const directive = {
+    link: link,
+    restrict: 'EA',
+    template: `
+      <div class="menu-button">
+        <a ui-sref="{{button.sref}}" ng-class="{'icon': button.iconClass}">
+          <i ng-if="button.iconClass" ng-class="button.iconClass" class="fa"></i>
+          <span>{{button.name}}</span>
+        </a>
+      </div>
+    `,
+  };
+  return directive;
 
-	function link(scope, element, attrs) {
-		scope.button = scope.$eval(attrs.data);
-	}
+  function link(scope, element, attrs) {
+    scope.button = scope.$eval(attrs.data);
+  }
 }
 
 angular
-	.module('RowdyMermaid-site.widgets')
-	.directive('rowdyMenuButton', rowdyMenuButton);
+  .module('RowdyMermaid-site.widgets')
+  .directive('rowdyMenuButton', rowdyMenuButton);
