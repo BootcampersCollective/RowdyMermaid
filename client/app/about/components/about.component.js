c5fc0574c59c9c17e43185d92fa194fe0d179530
Aaron Pennington <smokeyblues@hotmail.com>
2017-08-29 16:46:28 -0600

    re-added #88 content

--- client/app/about/components/about.component.js
+++ client/app/about/components/about.component.js
@@ -13,6 +13,12 @@ const about = {
         <div style="position: absolute; background-color: black; height: 50vh; width: 100vw;">
             <h1 style="text-align: center; color: white;">Hi I'm just a test div</h1>
         </div>
+        <div class="container" style="position: absolute; background-color: black; height: 50vh; width: 100vw;">
+        <span class="text">
+          Seitan tattooed occupy kinfolk raw denim brunch, vinyl photo booth pork belly. Austin vegan before they sold out edison bulb next level cronut unicorn ugh. Tousled XOXO gochujang edison bulb thundercats disrupt. Chillwave cloud bread everyday carry venmo distillery. Poutine yr schlitz artisan. Try-hard truffaut craft beer raclette blog marfa keffiyeh kombucha poutine YOLO retro celiac hell of banh mi. Man braid bushwick ramps meh hoodie. Gluten-free art party gentrify hot chicken. Activated charcoal intelligentsia ennui meditation. Single-origin coffee blue bottle XOXO, helvetica fixie four loko jean shorts small batch. Taxidermy brooklyn man bun kitsch franzen vaporware.
+        </span>
+        <span class="image">
+        </span>
     `
 };
 
