50020b782ececc059d33bb54d2b2a1133bb51323
Aaron Pennington <smokeyblues@hotmail.com>
2017-08-29 10:21:27 -0600

    cleaned up my console logs before pull request

--- client/app/widgets/parallax-jumbotron/parallax-jumbotron.directive.js
+++ client/app/widgets/parallax-jumbotron/parallax-jumbotron.directive.js
@@ -7,18 +7,13 @@ const parallaxJumbotron = {
       let ctrl = this;
 
       ctrl.$onInit = function() {
-          console.log('init ctrl.imageUrl: ', ctrl.imageUrl);
-          console.log('ctrl.title: ', ctrl.title);
       }
     },
     template: `
         <div class="parallax-container">
             <div class="image" style="background-image: url({{$ctrl.imageUrl}})"></div>
             <div class="title">{{$ctrl.title}}</div>
-            <!--<div>-->
-                <!--<img src="{{$ctrl.imageUrl}}" />-->
-            <!--</div>-->
-        <!--</div>-->
+
     `
 };
 
