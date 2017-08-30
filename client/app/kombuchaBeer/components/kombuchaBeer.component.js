b421bdfd116705dcb131d9bef33046dffbd377cb
Ryan Elliott <18182439+Ryan104@users.noreply.github.com>
2017-08-30 13:06:56 -0600

    Add beer info markup

--- client/app/kombuchaBeer/components/kombuchaBeer.component.js
+++ client/app/kombuchaBeer/components/kombuchaBeer.component.js
@@ -47,6 +47,19 @@ const kombuchaBeer = {
     template: `
     <parallax-jumbotron class="beerParralax" image-url="$ctrl.imageUrl" title="$ctrl.title"></parallax-jumbotron>
 
+    <article class="beer-info container">
+      <div class="row">
+        <img class="col-sm-6 img-responsive center-block img-rounded" src="https://d3ciwvs59ifrt8.cloudfront.net/9427377b-98ec-4581-86cb-348a0a0aed9a/ee1970f2-43c6-423f-8197-588c13a333e9_h.jpg?cache=new" alt="kombuchaBeer">
+        <h3 class="col-sm-6">WILD SOUR 100% WIND ENERGY KOMBUCHA BEER.</h3>
+      </div>
+      <div class="row">
+        <p>Earl Grey Kombucha is fermented for many weeks to the perfect pH and 
+        introduced to wild sour Belgian beer and the two of them skip hand in hand 
+        through a field of Swiss flowers. Available exclusively at our tasting room 
+        in limited supply. </p>
+      </div>  
+    </article>
+    
     <aside class="beer-gallery">
       <div ng-repeat="image in $ctrl.galleryImages" class="img-container">
         <div class="img" ng-style="{backgroundImage: 'url({{image.src}})'}"></div>
