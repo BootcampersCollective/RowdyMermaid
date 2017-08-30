d12974429499970191442ef62ad88c6ef4125b2e
Charlie Beck <charlie@bootcamperscollective.com>
2017-08-29 22:02:37 -0600

    add picture url

--- client/app/home/components/home.component.js
+++ client/app/home/components/home.component.js
@@ -5,6 +5,7 @@ const home = {
 	controller: function () {
 		let ctrl = this;
 		ctrl.title = 'Rowdy Mermaid';
+		ctrl.imageUrl = 'images/inHandRowdyBelly.jpg';
 
 		ctrl.$onInit = function () {
 			console.log('init Home');
@@ -20,12 +21,14 @@ const home = {
 		<h3>Rowdy Mermaid Kombucha</h3>
 		<h4>Boulder, Colorado</h4>
 	</div>
-	
+
 	<div class="viewFlavors">
 		<h3>Wild Landscape Inspired Sustainaby Sourced 100% Wind Energy Produced Living Adaptogenic Kombucha</h3>
 		<a href="/kombucha">View All Flavors</>
 	</div>
-	<parallax-jumbotron></parallax-jumbotron>
+	<!--TO DO: Update break to a padding bottom.-->
+	<br/>
+	<parallax-jumbotron image-url="$ctrl.imageUrl"></parallax-jumbotron>
 
 	`
 
