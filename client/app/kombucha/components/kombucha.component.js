524c838c39ea865eddd727db546f33cd86cbacd1
Jennifer Peavler <jenn.peavler@gmail.com>
2017-08-30 13:36:24 -0600

    deleted unnecessary commented out code

--- client/app/kombucha/components/kombucha.component.js
+++ client/app/kombucha/components/kombucha.component.js
@@ -82,15 +82,3 @@ angular.module('RowdyMermaid-site.kombucha')
     .component('kombucha', kombucha);
 
 kombucha.$inject = [];
-
-{/* <section class='kombucha-products'>
-  <article class='kombucha-flavor'>
-    <img class='ingredient-image' src='images/savoryPeaches.jpg' />
-    <img class='product-image' src='images/savoryPeachBottle.jpg' />
-    <p class='ingredients'>Filtered Colorado Snowmelt, Peach, Thyme Leaf.</p>
-    <div class='name-description'>
-      <p class='kombucha-name'>SAVORY PEACH</p>
-      <p class='kombucha-description'>No. 11 Immunity</p>
-    </div>
-  </article>
-</section> */}
