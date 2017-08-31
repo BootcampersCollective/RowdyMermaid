700cbe2919f6ad87695092b1eb6c0bb173a7dd86
RJ Russell <RJ-Russell@users.noreply.github.com>
2017-08-31 04:13:36 -0600

    add calendar header with left/right arrows and formatted current month and year

--- client/app/widgets/calendar/calendar.directive.js
+++ client/app/widgets/calendar/calendar.directive.js
@@ -2,8 +2,28 @@ function calendar() {
   return {
     link: link,
     restrict: 'E',
-    template: ``
+    template: `
+      <div id="cal-container">
+        <button class="fa fa-arrow-left"></button>
+        <span>{{ currDate.format("MMMM YYYY") }}</span>
+        <button class="fa fa-arrow-right"></button>
+      </div>
+    `
   };
 
-  function link(scope) {}
+  function link(scope) {
+    scope.dayNames = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday'
+    ];
+
+    scope.currDate = moment();
+  }
 }
+
+angular.module('RowdyMermaid-site.widgets').directive('calendar', calendar);
