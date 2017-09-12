ba3f2298dbc92aaeed5540f3e6c545ef1d1e1e2a
RJ Russell <RJ-Russell@users.noreply.github.com>
2017-09-12 04:33:47 -0600

    make form morer nicererer

--- client/app/widgets/contactForm/contactForm.component.js
+++ client/app/widgets/contactForm/contactForm.component.js
@@ -9,21 +9,23 @@ const contactForm = {
 		<div class="contact-form">
 			<div class="contact-form-header">Contact</div>
       <form>
-        <div>
-          <label for="contact-name">Name</label>
-          <input id="contact-name" type="text" name="contact-name" placeholder="Name">
-        </div>
-        <div>
-          <label for="email-address">Email</label>
-          <input id="email-address" type="email" name="email-address" placeholder="Email Address">
-        </div>
-        <div>
-          <label for="email-message" for="email-message">Message</label>
-          <textarea id="email-message" type="text" name="email-message"></textarea>
-        </div>
-        <div>
-          <button class="submit">Submit</button>
-        </div>
+        <ul>
+          <li>
+            <label for="contact-name">Name</label>
+            <input id="contact-name" type="text" name="contact-name" placeholder="Name" required>
+          </li>
+          <li>
+            <label for="email-address">Email</label>
+            <input id="email-address" type="email" name="email-address" placeholder="Email Address" required>
+          </li>
+          <li>
+            <label for="email-message">Message</label>
+            <textarea id="email-message" type="text" name="email-message" placeholder="Message" required></textarea>
+          </li>
+          <li>
+            <button class="submit">Submit</button>
+          </li>
+        </ul>
 			</form>
 		</div>
 	`
