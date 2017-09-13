62b774242ab99d93864d2d750a25eb5e91f07281
Charlie Beck <charlie@bootcamperscollective.com>
2017-09-13 17:27:39 -0600

    WIP: Added repeat to store component, minor styling.

--- client/app/store/components/store.component.js
+++ client/app/store/components/store.component.js
@@ -1,8 +1,25 @@
+const products = [
+    {
+        name: 'Rowdy Mermaid Trucker Hat',
+		price: 20,
+        productImgSrc: 'images/livingGingerProduct.jpg'
+    },
+    {
+        name: 'Rowdy Mermaid T-Shirt',
+        price: 20,
+        productImgSrc: 'images/livingGingerProduct.jpg'
+    }
+
+]
+
+
 const store = {
 	bindings: {},
 	controller: function () {
 		let ctrl = this;
 		ctrl.store = null;
+
+		ctrl.products = products;
 
 		ctrl.$onInit = function () {
 
@@ -10,6 +27,14 @@ const store = {
 
 	},
 	template: `
+	<section class='store-products'>
+        <article ng-repeat='product in $ctrl.products' class='products'>
+          <img class='product-image' src='{{product.productImgSrc}}' />
+          <p class='product-name'>{{product.name}} &#36{{product.price}}</p>
+          <button>Buy Now</button>
+          <button>Add to Cart</button>
+        </article>
+      </section>
 	`
 };
 
