90ca37336f166f676a6aa1863501dd223415ae7f
Aaron Pennington <smokeyblues@hotmail.com>
2017-10-05 12:02:21 -0600

    got the header component image to show. Need to adjust CSS and image dimensions.

--- client/app/widgets/header/header.component.js
+++ client/app/widgets/header/header.component.js
@@ -1,17 +1,21 @@
-const Header = {
+const header = {
     bindings: {
-        headerImage: '<',
+        headerImage: '<'
     },
     controller: function () {
         let ctrl = this;
 
         ctrl.$onInit = function () {
-
+            console.log("Header component loaded");
         }
     },
     template: `
     <div class="header-container">
-        <div class="header" style="background-image: url{{$ctrl.headerImage}}"></div>
+        <div class="image" style="background-image: url({{$ctrl.headerImage}})"></div>
     </div>
     `
 };
+
+angular
+    .module('RowdyMermaid-site.widgets')
+    .component('header', header);
