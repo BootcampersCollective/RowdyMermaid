1a597487c69a2a577fc9e328d2a5f0b987af07c9
Aaron Pennington <smokeyblues@hotmail.com>
2017-10-06 16:09:29 -0600

    finished issue 155

--- client/app/home/components/home.component.js
+++ client/app/home/components/home.component.js
@@ -15,6 +15,10 @@ const home = {
 		<div style="height: 80px;"></div> // in place to account for the navbar
 		<header header-image="'images/headerLogoCropped.png'"></header>
 		<header header-image="'images/taglineBanner.png'"></header>
+		<h1 style="text-align: center">Placeholder for carousel</h1>
+		<header header-image="'images/closeUpBottles.jpg'"></header>
+		<h1 style="text-align: center">Placeholder for reviews</h1>
+		<header header-image="'images/flatirons.png'"></header>
 	</div>
 	`
 
