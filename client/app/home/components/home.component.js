8a069a64893bc46201845b2a055d0ae9a51652e2
RJ Russell <RJ-Russell@users.noreply.github.com>
2017-10-10 23:19:54 -0600

    add top padding to body to offset fixed header

--- client/app/home/components/home.component.js
+++ client/app/home/components/home.component.js
@@ -1,30 +1,25 @@
 const home = {
-	bindings: {
+  bindings: {},
+  controller: function() {
+    let ctrl = this;
 
-	},
-	controller: function () {
-		let ctrl = this;
-
-		ctrl.$onInit = function () {
-			console.log('init Home');
-		};
-		ctrl.$postLink = function () {};
-	},
-	template: `
-	<div class="home-container">
-		<div style="height: 80px;"></div> // in place to account for the navbar
+    ctrl.$onInit = function() {
+      console.log('init Home');
+    };
+    ctrl.$postLink = function() {};
+  },
+  template: `
+	<!-- <div class="home-container">
 		<header header-image="'images/headerLogoCropped.png'"></header>
 		<header header-image="'images/taglineBanner.png'"></header>
 		<h1 style="text-align: center">Placeholder for carousel</h1>
 		<header header-image="'images/closeUpBottles.jpg'"></header>
 		<h1 style="text-align: center">Placeholder for reviews</h1>
 		<header header-image="'images/flatirons.png'"></header>
-	</div>
+	</div> -->
 	`
-
 };
 
-angular.module('RowdyMermaid-site.home')
-	.component('home', home);
+angular.module('RowdyMermaid-site.home').component('home', home);
 
 home.$inject = [];
