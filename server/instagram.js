6087767083e40ee19ed9a3a33a520f5cc8d81de4
Charlie Beck <charlie@bootcamperscollective.com>
2017-10-20 18:37:43 -0600

    WIP

--- server/instagram.js
+++ server/instagram.js
@@ -1,9 +1,1 @@
-let constObj = {
-    APP_ACCESS_TOKEN: '1264789626.971ecd9.3cc1498dc3e24f0c8274f8699a9538bb',
-};
 
-let INSTA_API_CONSTANTS = Object.freeze(constObj);
-
-module.exports = {
-    INSTA_API_CONSTANTS
-};
